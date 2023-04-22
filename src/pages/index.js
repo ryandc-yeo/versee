6a035d482c85d211eb4d4328fe5241f95ccf190e
Andrew Huynh <61716827+andhuynh@users.noreply.github.com>
2023-04-22 14:43:08 -0700

    added camera functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Main from './Home'
+import CameraPage from './Camera'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <CameraPage />
       <Main />
     </>
   )
