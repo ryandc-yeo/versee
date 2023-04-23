493615b57c7dbbb1db712c2fe39117dd95ad43e3
Ryan Yeo <ryandc.yeo@gmail.com>
2023-04-23 03:31:00 -0700

    not out of context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Main from "./Home";
-import Cohere from "./Cohere";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Cohere />
+      <Main />
     </div>
   );
 }
